59efb07822df0a218b88d97fa0f8487643750932
deathcap <deathcap@gmx.co.uk>
2015-02-08 17:40:15 -0800

    Transform to replace rfile() with raw file data directly

--- demo.js
+++ demo.js
@@ -1,8 +1,27 @@
 var browserify = require('browserify');
+var brfs = require('brfs');
+var through = require('through');
 
 require('browserify/lib/builtins').fs = 'create-webfs.js'; // TODO: find a better way to replace this module
 
 var b = browserify();
+
+// directly include rfile TODO: use brfs, but it replaces fs.readFileSync
+b.transform(function(s) {
+  var data = '';
+  return through(write, end);
+
+  function write(buf) { data += buf; }
+  function end() {
+    var fs = require('fs');
+    var s = data;
+    var includedFile = fs.readFileSync('node_modules//browserify/node_modules/umd/template.js'); // TODO: encoding? string?
+    s = s.replace(/rfile\('.\/template.js'\)/, JSON.stringify(includedFile));
+    this.queue(s);
+    this.queue(null);
+  };
+}, { global: true });
+
 b.add('./demo.js');
 b.bundle().pipe(process.stdout);
 
